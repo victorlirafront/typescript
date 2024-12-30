465b8053fb5fa00e27b7e3cba81b8471e0ee51f2
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-30 12:14:12 -0300

    feat: add primitive types

---
+++ primitive-types/main.ts
@@ -0,0 +1,7 @@
+// Primitive Types
+let userName: string = "Victor"; // Text
+let userAge: number = 30; // Number
+let userisActive: boolean = true; // Boolean
+
+let emptyValue: null = null; // Null value
+let notDefined: undefined = undefined; // Undefined value
